88c511b6625038618204d992f0225a9ab2d8bb2a
Kanji Yomoda <kanji.yy@gmail.com>
2017-09-09 20:03:19 -0700

    Create action creater for searching weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'ae649445267a5a98639df4da54137090';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
